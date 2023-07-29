bde5808f5175259de6ae7f40ede2a50d2140b7d7
Yash <yinani@ucdavis.edu>
2023-07-29 10:29:49 -0700

    backend changes

--- backend/server.go
+++ backend/server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"mat-back/graph"
 	"net/http"
+	// "os"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 )
@@ -11,6 +12,10 @@ import (
 const defaultPort = "8080"
 
 func main() {
+	// port := os.Getenv("PORT")
+	// if port == "" {
+	// 	port = defaultPort
+	// }
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
