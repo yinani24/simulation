f70e5d73ea500cbcaf9a7b46c77c4c59982f0805
Yash <yinani@ucdavis.edu>
2023-08-08 11:15:50 -0700

    tailwindcss updates and ant design

--- frontend/matrix/src/components/simulation.tsx
+++ frontend/matrix/src/components/simulation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
-
+import {Button} from 'antd'
 interface MatrixData {
     _id: string;
     name: string;
@@ -16,21 +16,23 @@ function Simulation({ matrixData, handleMatrixAdd, handleMatrixRemove }: {
         localStorage.setItem("matrix_id", id);
     }  
     return(
-        <>
-        <ul>
-            {matrixData.map((matrix) => (
-                <li key={matrix._id}>
-                    <button key={matrix._id} onClick={() => matrix_id_storage(matrix._id)}>
-                        <Link to={`/${matrix.name}`}>
-                            {matrix.name}
-                        </Link>
-                    </button>
-                </li>
-            ))}
-        </ul>
-        <button onClick={handleMatrixAdd}>Add Button</button>
-        <button onClick={() => handleMatrixRemove(matrixData[matrixData.length - 1]?._id)}>Remove Button</button>
-        </>
+        <div className="w-full h-full">
+            <div className="w-full h-full flex flex-row justify-center content-center">
+                <Button  onClick={() => handleMatrixRemove(matrixData[matrixData.length - 1]?._id)}>Remove Button</Button>
+                <ul>
+                    {matrixData.map((matrix) => (
+                        <li key={matrix._id}>
+                            <button key={matrix._id} onClick={() => matrix_id_storage(matrix._id)}>
+                                <Link to={`/${matrix.name}`}>
+                                    {matrix.name}
+                                </Link>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+                <Button onClick={handleMatrixAdd}>Add Button</Button>
+            </div>
+        </div>
     )
 }
 
---
+++ frontend/matrix/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    extend: {
+      spacing: {
+        "1/2": "50%",
+        "1/3": "33.333333%",
+        "2/3": "66.666667%",
+        "1/4": "25%",
+      },
+      
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
